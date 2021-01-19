27ab38e030559acc441187cd6f38a5ad23ea5465
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2021-01-19 16:21:59 +1000

    edgecases - find analytically?

--- edgecases.py
+++ edgecases.py
@@ -3,12 +3,14 @@
 """
     File to find the edge cases where the condition is given
     for unit quaternions.
-        
+        function theta_norm is the one that throws the error
+        theta_norm = 2 / np.sqrt(1 - 4 * ((quat[1] * quat[2] + quat[0] * quat[3])**2)) 
+        Find all values where theta_norm will throw a divide by 0 error!
+        Also, the other denominators might throw errors       
 """
 
 from .cov import randomQuat
-
-
+import numpy as np
 
 
 if __name__=="__main__":
